a4e9cd7e8eb1e9f0368f153777350056970d3e8f
A.S. <126242006+AndrejFinki@users.noreply.github.com>
2024-01-06 16:23:19 +0000

    Add sync() function. This function forces all MPI processes to sync at one spot.

--- src/driver.cpp
+++ src/driver.cpp
@@ -8,13 +8,16 @@ using namespace std;
 #include "timer.hpp"
 #include "compression.hpp"
 
+const string data_dir = "../ECG_Parallel_Compression/data/";
+const string output_dir = "../ECG_Parallel_Compression/output/";
+
 int main(
     int argc,
     char ** argv
 ) {
     const string file_name = argv[1];
-    const string file_name_data = "../ECG_Parallel_Compression/data/" + file_name;
-    const string file_name_output = "../ECG_Parallel_Compression/output/" + file_name;
+    const string file_name_data = data_dir + file_name;
+    const string file_name_output = output_dir + file_name;
 
     MPI_Handler::mpi_init();
 
@@ -23,6 +26,8 @@ int main(
     Timer *timer_compression = ( MPI_Handler::get_rank() ? nullptr : new Timer( "ECG Total Compression Time" ) );
 
     MPI_Handler::run( file_name_data, file_name_output );
+
+    MPI_Handler::sync();
 
     delete timer_compression;
 
