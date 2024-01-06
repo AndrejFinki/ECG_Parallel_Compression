5095271244f60ec469f4b51bd91a9a3cd1c945ed
A.S. <126242006+AndrejFinki@users.noreply.github.com>
2024-01-06 13:44:16 +0000

    Refactor DataHandler

--- src/data_handler.hpp
+++ src/data_handler.hpp
@@ -9,42 +9,25 @@ class DataHandler {
 public:
 
     DataHandler(
-        int argc,
-        char ** argv
+        string file_path_arg
     ) {
-        assert( argc >= 2 && "DataHandler constructor failed! Missing data file path." );
-        data_file_name = argv[1];
-        data_path = data_directory + data_file_name;
-        check_file_path( data_path );
+        file_path = file_path_arg;
     }
 
-    const vector <int> * get_ecg_data();
-    void read_data();
-    void write_output( const vector <int> & );
+    const vector <int> * read();
+    void write( const vector <int> * );
 
 private:
 
-    const string data_directory = "../ECG_Parallel_Compression/data/";
-    const string output_directory = "../ECG_Parallel_Compression/output/";
-    string data_path = "";
-    string data_file_name = "";
+    string file_path;
     vector <int> ecg_data;
-
-    void check_file_path( const string & );
 
 };
 
-const vector <int> * DataHandler::get_ecg_data()
-{
-    if( ecg_data.empty() ) return nullptr;
-    const vector <int> * ecg_data_cptr = &ecg_data;
-    return ecg_data_cptr;
-}
-
-void DataHandler::read_data()
+const vector <int> * DataHandler::read()
 {
     fstream file;
-    file.open( data_path, fstream::in );
+    file.open( file_path, fstream::in );
 
     assert( file && "Read data file check failed!" );
 
@@ -54,25 +37,21 @@ void DataHandler::read_data()
     while( file >> value ) ecg_data.push_back( value );
 
     file.close();
+
+    return &ecg_data;
 }
 
-void DataHandler::check_file_path(
-    const string &path
+void DataHandler::write(
+    const vector <int> * data
 ) {
-    fstream file;
-    file.open( path, fstream::in );
+    fstream file( file_path, fstream::out );
 
-    assert( file && "File path check failed!" );
-
-    file.close();
-}
-
-void DataHandler::write_output(
-    const vector <int> &data
-) {
-    fstream file( output_directory + data_file_name + ".cmp", fstream::out );
     assert( file && "Output file write failed!" );
-    for( const int &i : data ) file << i << endl;
+
+    for( int i = 0 ; i < data->size() ; i++ ) {
+        file << data->at(i) << endl;
+    }
+
     file.close();
 }
 
