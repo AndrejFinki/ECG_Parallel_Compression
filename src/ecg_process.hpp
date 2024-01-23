f71d3eeee26a05ce54f9355aa1a131d0e0164228
A.S. <126242006+AndrejFinki@users.noreply.github.com>
2024-01-23 13:42:05 +0000

    Create ECG_Process_Method_2 class

--- src/ecg_process.hpp
+++ src/ecg_process.hpp
@@ -61,4 +61,52 @@ void ECG_Process_Method_1::secondary_process()
     MPI_Gather( pdata.data(), data_per_process, MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD );
 }
 
+class ECG_Process_Method_2 : public ECG_Process {
+public:
+    vector <int> main_process( const vector <int> *, const int & );
+    void secondary_process();
+};
+
+vector <int> ECG_Process_Method_2::main_process(
+    const vector <int> * data,
+    const int &size
+) {
+    int data_per_process = data->size() / size;
+    MPI_Bcast( &data_per_process, 1, MPI_INT, 0, MPI_COMM_WORLD );
+
+    vector <int> pdata( data_per_process );
+    MPI_Scatter( data->data(), data_per_process, MPI_INT, pdata.data(), data_per_process, MPI_INT, 0, MPI_COMM_WORLD );
+
+    Compression::inplace_compress( pdata );
+
+    vector <int> sq_data( data_per_process * size );
+    MPI_Gather( pdata.data(), data_per_process, MPI_INT, sq_data.data(), data_per_process, MPI_INT, 0, MPI_COMM_WORLD );
+
+    if( data_per_process * size != data->size() ) {
+        vector <int> extra_data;
+        for( int i = data_per_process * size ; i < data->size() ; i++ ) {
+            extra_data.push_back( data->at(i) );
+        }
+        Compression::inplace_compress( extra_data );
+        for( int &i : extra_data ) {
+            sq_data.push_back( i );
+        }
+    }
+
+    return sq_data;
+}
+
+void ECG_Process_Method_2::secondary_process()
+{
+    int data_per_process;
+    MPI_Bcast( &data_per_process, 1, MPI_INT, 0, MPI_COMM_WORLD );
+
+    vector <int> pdata( data_per_process );
+    MPI_Scatter( NULL, 0, MPI_INT, pdata.data(), data_per_process, MPI_INT, 0, MPI_COMM_WORLD );
+
+    Compression::inplace_compress( pdata );
+    
+    MPI_Gather( pdata.data(), data_per_process, MPI_INT, NULL, 0, MPI_INT, 0, MPI_COMM_WORLD );
+}
+
 #endif
